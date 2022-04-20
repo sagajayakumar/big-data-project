fb2836578c7448d37d418f8782e883472d83b1db
SHIVA RAM KOTHAPALLY <77807605+shivaramkothapally@users.noreply.github.com>
2022-04-19 21:14:20 -0500

    job 1 finalizer

---
+++ members/Shiva/page-rank-beam/src/main/java/edu/nwmissouri/beam/pagerank/votingpage.java
@@ -0,0 +1,33 @@
+package main.java.edu.nwmissouri.beam.pagerank;
+
+import java.io.Serializable;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.values.KV;
+
+public  class VotingPage extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPage>> implements Serializable{
+    String voterName;
+    int contributorVotes;
+    public VotingPage(String voterName,Integer contributorVotes2){
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes2;        
+    }
+    public String getVoterName() {
+        return voterName;
+    }
+    public void setVoterName(String voterName) {
+        this.voterName = voterName;
+    }
+    public int getContributorVotes() {
+        return contributorVotes;
+    }
+    public void setContributorVotes(int contributorVotes) {
+        this.contributorVotes = contributorVotes;
+    }
+    @Override
+    public String toString() {
+        return "contributorVotes=" + contributorVotes + ", voterName=" + voterName;
+    }
+
+
+}
