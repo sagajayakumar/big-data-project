3fca96286074d0e641b39e745e2542b940857137
Jaya Kumar <jayakumar1994@gmail.com>
2022-04-29 19:29:10 -0500

    updated job2 and job3 for page rank

--- members/Jaya/page-rank-beam/src/main/java/edu/nwmissouri/beam/pagerank/MinimalPageRankJaya.java
+++ members/Jaya/page-rank-beam/src/main/java/edu/nwmissouri/beam/pagerank/MinimalPageRankJaya.java
@@ -1,6 +1,13 @@
 
 package edu.nwmissouri.beam.pagerank;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+
+import com.fasterxml.jackson.databind.type.CollectionLikeType;
+
 // beam-playground:
 //   name: MinimalWordCount
 //   description: An example that counts words in Shakespeare's works.
@@ -12,14 +19,12 @@ package edu.nwmissouri.beam.pagerank;
 //     - IO
 //     - Core Transforms
 
-import java.util.Arrays;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
@@ -27,6 +32,7 @@ import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
 import org.apache.beam.sdk.values.TypeDescriptors;
+import org.yaml.snakeyaml.events.CollectionEndEvent;
 
 public class MinimalPageRankJaya {
 
@@ -46,11 +52,10 @@ public class MinimalPageRankJaya {
     PCollection<KV<String, String>> mergedList = result.apply(Flatten.<KV<String, String>>pCollections());
 
     PCollection<KV<String, Iterable<String>>> group = mergedList.apply(GroupByKey.create());
-    PCollection<String> pckvStrings = group.apply(
-        MapElements.into(
-            TypeDescriptors.strings())
-            .via((resultMergeLstout) -> resultMergeLstout.toString()));
-    pckvStrings.apply(TextIO.write().to("jayacounts"));
+    PCollection<String> pLinksStr = group
+        .apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut) -> mergeOut.toString()));
+
+    pLinksStr.apply(TextIO.write().to("jayacounts"));
     p.run().waitUntilFinish();
   }
 
@@ -71,4 +76,90 @@ public class MinimalPageRankJaya {
             .via(linelink -> KV.of(dataFile, linelink)));
     return kv;
   }
+
+  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<String>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer contributorVotes = 0;
+      if (element.getValue() instanceof Collection) {
+        contributorVotes = ((Collection<String>) element.getValue()).size();
+      }
+      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      for (String voterName : element.getValue()) {
+        if (!voterName.isEmpty()) {
+          voters.add(new VotingPage(voterName, contributorVotes));
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+    }
+  }
+
+  static class Job2Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {
+
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPage> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer votes = 0;
+      ArrayList<VotingPage> voters = element.getValue().getVoted();
+      if (voters instanceof Collection) {
+        votes = ((Collection<VotingPage>) voters).size();
+      }
+
+      for (VotingPage votingPage : voters) {
+        String pageName = votingPage.getVoterName();
+        Double pageRank = votingPage.getVoterRank();
+        String contributingPageName = element.getKey();
+        Double contributingPageRank = element.getValue().getRank();
+        VotingPage contributor = new VotingPage(contributingPageName, contributingPageRank, votes);
+        ArrayList<VotingPage> arr = new ArrayList<>();
+        arr.add(contributor);
+        receiver.output(KV.of(votingPage.getVoterName(), new RankedPage(pageName, pageRank, arr)));
+      }
+    }
+
+  }
+
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {
+
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<RankedPage>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      String thisPage = element.getKey();
+      Iterable<RankedPage> rankedPages = element.getValue();
+      Double dampingFactor = 0.85;
+      Double updatedRank = (1 - dampingFactor);
+      ArrayList<VotingPage> newVoters = new ArrayList<VotingPage>();
+
+      for (RankedPage rankedPage : rankedPages) {
+        if (rankedPage != null) {
+          for (VotingPage votePage : rankedPage.getVoted()) {
+            newVoters.add(votePage);
+            updatedRank += (dampingFactor) * votePage.getVoterRank() / (double) votePage.getVoterRank();
+          }
+        }
+      }
+
+      receiver.output(KV.of(thisPage, new RankedPage(thisPage, updatedRank, newVoters)));
+    }
+  }
+
+  static class Job3Finder extends DoFn<KV<String, RankedPage>, KV<String, Double>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPage> element,
+        OutputReceiver<KV<String, Double>> receiver) {
+      String currentPage = element.getKey();
+      Double currentPageRank = element.getValue().getRank();
+
+      receiver.output(KV.of(currentPage, currentPageRank));
+    }
+  }
+
+  public static class Job3Final implements Comparator<KV<String, Double>>, Serializable {
+    @Override
+    public int compare(KV<String, Double> o1, KV<String, Double> o2) {
+      return o1.getValue().compareTo(o2.getValue());
+    }
+  }
+
 }
--- members/Jaya/page-rank-beam/src/main/java/edu/nwmissouri/beam/pagerank/VotingPage.java
+++ members/Jaya/page-rank-beam/src/main/java/edu/nwmissouri/beam/pagerank/VotingPage.java
@@ -1,6 +1,55 @@
 package edu.nwmissouri.beam.pagerank;
 
-public class VotingPage {
+import java.io.Serializable;
+
+public class VotingPage implements Serializable {
+    private String voterName = "";
+    private double voterRank = 0.0;
+    private Integer contributorVotes = 0;
+
     public VotingPage(String voterName, Integer contributorVotes) {
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes;
     }
+
+    public VotingPage(String voterName, double voterRank) {
+        this.voterName = voterName;
+        this.voterRank = voterRank;
+    }
+
+    public VotingPage(String voterName, double voterRank, Integer contributorVotes) {
+        this.voterName = voterName;
+        this.voterRank = voterRank;
+        this.contributorVotes = contributorVotes;
+    }
+
+    public String getVoterName() {
+        return voterName;
+    }
+
+    public void setVoterName(String voterName) {
+        this.voterName = voterName;
+    }
+
+    public double getVoterRank() {
+        return voterRank;
+    }
+
+    public void setVoterRank(double voterRank) {
+        this.voterRank = voterRank;
+    }
+
+    public Integer getContributorVotes() {
+        return contributorVotes;
+    }
+
+    public void setContributorVotes(Integer contributorVotes) {
+        this.contributorVotes = contributorVotes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.4f %s", voterName, voterRank, contributorVotes);
+    }
+
 }
